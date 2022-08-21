1c4fa1a1cf872c55e91933ab3a06da7c2cc7592b
Mmd Ramzi <sara.jalal.115@gmail.com>
2022-08-21 17:49:17 +0430

    plans added

---
+++ src/assets/svg/PlansArrowButton.js
@@ -0,0 +1,5 @@
+export default () => (
+<svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.0832 11.12L2.27987 7.31667C1.8307 6.8675 1.8307 6.1325 2.27987 5.68334L6.0832 1.88" stroke="white" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+)
